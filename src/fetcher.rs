9e8831845cd6a4b61c262765a983b2c4a84c40f1
Benign X <1341398182@qq.com>
2024-04-28 10:30:14 +0800

    😁(update): update
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -3,21 +3,94 @@ extern crate serde_json;
 
 use std::fmt::{Display, Error, Formatter};
 use reqwest::Response;
+use serde::{Deserialize, Serialize};
 
 const PROBLEMS_URL: &str = "https://leetcode.cn/api/problems/algorithms";
 const GRAPHQL_URL: &str = "https://leetcode.cn/graphql";
 
 const QUESTION_QUERY_OPERATION: &str = "questionData";
 
-pub async fn get_problems() -> Option<String> {
+pub async fn get_problems() -> Option<Problems> {
     let client = reqwest::Client::new();
-    let res = client.get(PROBLEMS_URL)
-        .send()
-        .await.ok();
-    match res {
-        None => { None }
-        Some(res) => {
-            res.text().await.ok()
+    client
+        .get(PROBLEMS_URL)
+        .send().await.ok()?
+        .json().await.ok()?
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Problems {
+    user_name: String,
+    num_solved: u32,
+    num_total: u32,
+    ac_easy: u32,
+    ac_medium: u32,
+    ac_hard: u32,
+    pub stat_status_pairs: Vec<StatWithStatus>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StatWithStatus {
+    pub stat: Stat,
+    difficulty: Difficulty,
+    paid_only: bool,
+    is_favor: bool,
+    frequency: u32,
+    progress: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Stat {
+    question_id: u32,
+    #[serde(rename = "question__article__slug")]
+    question_article_slug: Option<String>,
+    #[serde(rename = "question__title")]
+    question_title: Option<String>,
+    #[serde(rename = "question__title_slug")]
+    question_title_slug: Option<String>,
+    #[serde(rename = "question__hide")]
+    question_hide: bool,
+    total_acs: u32,
+    total_submitted: u32,
+    pub frontend_question_id: String,
+    is_new_question: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Difficulty {
+    level: u32,
+}
+
+impl Display for Difficulty {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match self.level {
+            1 => f.write_str("Easy"),
+            2 => f.write_str("Medium"),
+            3 => f.write_str("Hard"),
+            _ => f.write_str("Unknown"),
         }
     }
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Problem {
+    pub title: String,
+    pub title_slug: String,
+    pub content: String,
+    #[serde(rename = "codeDefinition")]
+    pub code_definition: Vec<CodeDefinition>,
+    #[serde(rename = "sampleTestCase")]
+    pub sample_test_case: String,
+    pub difficulty: String,
+    pub question_id: u32,
+    pub return_type: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CodeDefinition {
+    pub value: String,
+    pub text: String,
+    #[serde(rename = "defaultCode")]
+    pub default_code: String,
 }
