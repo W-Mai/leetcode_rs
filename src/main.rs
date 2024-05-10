d7d89dd4110bf9b2b020ffc3d1f32557b980fb97
Benign X <1341398182@qq.com>
2024-05-10 17:30:46 +0800

    😁(main): download problems
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod fetcher;
 
 use askama::Template;
 use futures::executor::block_on;
+use futures::FutureExt;
 
 #[derive(Template)]
 #[template(path = "template")]
@@ -49,33 +50,43 @@ impl QuestionTemplate {
 #[tokio::main]
 async fn main() {
     if let Some(problems) = block_on(fetcher::get_problems()) {
-        let first = problems.stat_status_pairs.first();
-        if let Some(first) = first {
-            let problem = block_on(fetcher::get_problem(first));
-            if let Some(problem) = problem {
-                println!("{}", problem.title);
-                let file_name = format!("{}-{}", problem.title_slug, problem.title);
-                let question_id = problem.question_id.replace(" ", "_");
-                QuestionTemplate::new(
-                    question_id.clone(),
-                    problem.title,
-                    problem.content,
-                    format!("https://leetcode.com/problems/{}/", problem.title_slug),
-                    format!(
-                        "https://leetcode.com/problems/{}/discuss/",
-                        problem.title_slug
-                    ),
-                    "".to_string(),
-                    problem
-                        .code_definition
-                        .iter()
-                        .find(|x| x.value == "rust")
-                        .unwrap()
-                        .default_code
-                        .clone(),
-                )
-                .write(format!("./tests/{}-{}.rs", question_id, file_name).as_ref());
-            }
+        for problem in problems.stat_status_pairs {
+            fetcher::get_problem(&problem)
+                .then(|problem| async {
+                    if let Some(problem) = problem {
+                        let question_id = problem.question_id;
+                        let file_name = format!("{}-{}", problem.title_slug, problem.title)
+                            .replace(" ", "_")
+                            .replace(".", "_")
+                            .replace("?", "")
+                            .replace("!", "")
+                            .replace("(", "_")
+                            .replace(")", "_");
+
+                        let file_name = format!("./tests/{}-{}.rs", question_id, file_name);
+                        let code_definition =
+                            problem.code_definition.iter().find(|x| x.value == "rust");
+                        if code_definition.is_none() {
+                            return;
+                        }
+                        println!("{}", file_name);
+
+                        QuestionTemplate::new(
+                            question_id.clone(),
+                            problem.title,
+                            problem.content,
+                            format!("https://leetcode.com/problems/{}/", problem.title_slug),
+                            format!(
+                                "https://leetcode.com/problems/{}/discuss/",
+                                problem.title_slug
+                            ),
+                            "".to_string(),
+                            code_definition.unwrap().default_code.clone(),
+                        )
+                        .write(file_name.as_ref());
+                    }
+                })
+                .await;
         }
     }
 }
