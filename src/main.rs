8c10d7fea50f2d6c3128d83b749d56b84cbab278
Benign X <1341398182@qq.com>
2024-05-10 17:38:18 +0800

    😁(main): try to async
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod fetcher;
 
 use askama::Template;
-use futures::executor::block_on;
+use futures::executor::{block_on, ThreadPool};
+use futures::task::SpawnExt;
 use futures::FutureExt;
 
 #[derive(Template)]
@@ -50,43 +51,57 @@ impl QuestionTemplate {
 #[tokio::main]
 async fn main() {
     if let Some(problems) = block_on(fetcher::get_problems()) {
+        let pool = ThreadPool::new().unwrap();
+
+        let mut tasks = vec![];
+
         for problem in problems.stat_status_pairs {
-            fetcher::get_problem(&problem)
-                .then(|problem| async {
-                    if let Some(problem) = problem {
-                        let question_id = problem.question_id;
-                        let file_name = format!("{}-{}", problem.title_slug, problem.title)
-                            .replace(" ", "_")
-                            .replace(".", "_")
-                            .replace("?", "")
-                            .replace("!", "")
-                            .replace("(", "_")
-                            .replace(")", "_");
+            tasks.push(
+                pool.spawn_with_handle(async move {
+                    fetcher::get_problem(&problem)
+                        .then(|problem| async {
+                            if let Some(problem) = problem {
+                                let question_id = problem.question_id;
+                                let file_name = format!("{}-{}", problem.title_slug, problem.title)
+                                    .replace(" ", "_")
+                                    .replace(".", "_")
+                                    .replace("?", "")
+                                    .replace("!", "")
+                                    .replace("(", "_")
+                                    .replace(")", "_");
 
-                        let file_name = format!("./tests/{}-{}.rs", question_id, file_name);
-                        let code_definition =
-                            problem.code_definition.iter().find(|x| x.value == "rust");
-                        if code_definition.is_none() {
-                            return;
-                        }
-                        println!("{}", file_name);
+                                let file_name = format!("./tests/{}-{}.rs", question_id, file_name);
+                                let code_definition =
+                                    problem.code_definition.iter().find(|x| x.value == "rust");
+                                if code_definition.is_none() {
+                                    return;
+                                }
+                                println!("{}", file_name);
 
-                        QuestionTemplate::new(
-                            question_id.clone(),
-                            problem.title,
-                            problem.content,
-                            format!("https://leetcode.com/problems/{}/", problem.title_slug),
-                            format!(
-                                "https://leetcode.com/problems/{}/discuss/",
-                                problem.title_slug
-                            ),
-                            "".to_string(),
-                            code_definition.unwrap().default_code.clone(),
-                        )
-                        .write(file_name.as_ref());
-                    }
+                                QuestionTemplate::new(
+                                    question_id.clone(),
+                                    problem.title,
+                                    problem.content,
+                                    format!(
+                                        "https://leetcode.com/problems/{}/",
+                                        problem.title_slug
+                                    ),
+                                    format!(
+                                        "https://leetcode.com/problems/{}/discuss/",
+                                        problem.title_slug
+                                    ),
+                                    "".to_string(),
+                                    code_definition.unwrap().default_code.clone(),
+                                )
+                                .write(file_name.as_ref());
+                            }
+                        })
+                        .await;
                 })
-                .await;
+                .unwrap(),
+            );
         }
+
+        futures::future::join_all(tasks).await;
     }
 }
